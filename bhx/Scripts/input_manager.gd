9396fa47400dc9da21297f1343bc1b39a4c974c9
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2025-01-11 05:21:44 -0500

    PLANE j<en ai marre

--- bhx/Scripts/input_manager.gd
+++ bhx/Scripts/input_manager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var tilemap: TileManager = $"../Tilemap"
+@onready var plane_script: Plane1 =$"../Plane"
 
 enum SelectingType {
 	NONE,
@@ -21,6 +22,17 @@ func _input(event: InputEvent) -> void:
 	elif Input.is_action_just_pressed("Click") and selecting_type == SelectingType.DESTROY1:
 		destroy()
 		selecting_type = SelectingType.NONE
+	elif Input.is_action_just_pressed("Click") and selecting_type == SelectingType.PLANE1:
+		plane_script.plane_function(tilemap.map, get_clicked_tile().position)
+		selecting_type = SelectingType.NONE
+
+func get_clicked_tile() -> Tile:
+	var coords: Vector2i = tilemap.ground.local_to_map(tilemap.ground.get_local_mouse_position()) + tilemap.offset
+	if coords.y % 2 == 0:
+		coords += Vector2i(0, 1)
+	else:
+		coords += Vector2i(0, 1)
+	return tilemap.get_tile_from_position(tilemap.map, coords)
 
 func destroy() -> void:
 	var coords: Vector2i = tilemap.ground.local_to_map(tilemap.ground.get_local_mouse_position()) + tilemap.offset
