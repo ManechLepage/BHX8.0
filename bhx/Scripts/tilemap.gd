c9bbeadd0322cccef80a0101628b50d79d6e983f
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2025-01-11 12:15:27 -0500

    Things dry

--- bhx/Scripts/tilemap.gd
+++ bhx/Scripts/tilemap.gd
@@ -137,7 +137,7 @@ func load_water() -> void:
 	for x in range(256):
 		for y in range(256):
 			if not ground.get_cell_tile_data(Vector2i(x, y) + offset - Vector2i(128, 128)):
-				ground.set_cell(Vector2i(x, y) + offset - Vector2i(128, 128), 0, Vector2i(0, 2))
+				ground.set_cell(Vector2i(x, y) + offset - Vector2i(128, 128), 0, Vector2i(dryness_level, 2))
 
 func get_tile_type(position: Vector2i) -> Gamemanager.TileType:
 	var used_position: Vector2 = Vector2(position.x, position.y / 2)
