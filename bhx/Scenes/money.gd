d3778b6c3a844619db67081e16d1816b3c07748a
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-01-11 03:07:09 -0500

    JIDDLE

--- bhx/Scenes/money.gd
+++ bhx/Scenes/money.gd
@@ -4,15 +4,9 @@ var money: int
 @onready var money_label: Label = $Money
 @onready var money_timer: Timer = $Money/Money_timer
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	money = 100
 	money_label.text = str(money)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func _on_money_timer_timeout() -> void:
