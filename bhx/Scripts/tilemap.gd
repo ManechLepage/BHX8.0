228bd7fd69869c0c0efd3484ed3a526918e3ce80
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2025-01-11 00:29:02 -0500

    Rivers

--- bhx/Scripts/tilemap.gd
+++ bhx/Scripts/tilemap.gd
@@ -46,6 +46,8 @@ func generate() -> Array[Tile]:
 			tile.position = position
 			generated_map.append(tile)
 	
+	generated_map = add_rivers(generated_map)
+	
 	return generated_map
 
 func get_tile_type(position: Vector2i) -> Gamemanager.TileType:
@@ -85,3 +87,62 @@ func update_tile_map(tiles: Array[Tile]) -> void:
 		ground.set_cell(tile.position + offset, 0, atlas_coords)
 		if tile.type == Gamemanager.TileType.FOREST:
 			ground_cover.set_cell(tile.position + offset, 0, Vector2i(0, 0))
+
+func get_tile_from_position(generated_map: Array[Tile], position: Vector2i) -> Tile:
+	var index: int = position.y * params.dimensions.x + position.x
+	return generated_map[index]
+
+func get_random_water(water_tiles: Array[Tile]) -> Tile:
+	return water_tiles.pick_random()
+
+func get_all_water_tiles(generated_map: Array[Tile]) -> Array[Tile]:
+	var all_waters: Array[Tile] = []
+	for tile in generated_map:
+		all_waters.append(tile)
+	return all_waters
+
+func get_neighbour_from_angle(generated_map: Array[Tile], tile: Tile, angle: float) -> Tile:
+	# Angle values: [-1; 1]
+	angle *= 180 / 90  # range [-2, 2]
+	angle = round(angle)
+	var all_neighbours: Array[Vector2i] = ground.get_surrounding_cells(tile.position)
+	var index: int = angle + 2
+	
+	if index >= all_neighbours.size():
+		index = randi_range(0, all_neighbours.size() - 1)
+	
+	var tile_position: Vector2i = all_neighbours[index]
+	return get_tile_from_position(generated_map, tile_position)
+	
+
+func add_rivers(generated_map: Array[Tile]) -> Array[Tile]:
+	var number_of_rivers: int = randi_range(5, 20)
+	var all_water_tiles = get_all_water_tiles(generated_map)
+	
+	for i in range(number_of_rivers):
+		var start_water: Tile = get_random_water(all_water_tiles)
+		var start_direction: float
+		
+		var direction_noise: FastNoiseLite = FastNoiseLite.new()
+		direction_noise.seed = params.seed + 10 * i
+		direction_noise.noise_type = FastNoiseLite.TYPE_PERLIN
+		
+		var direction: float
+		var step: int = 0
+		var number_of_land_tiles: int = 0
+		while true:
+			direction = direction_noise.get_noise_1d(step)
+			var neighbour_in_direction: Tile = get_neighbour_from_angle(generated_map, start_water, start_direction)
+			step += 1
+			
+			start_water = neighbour_in_direction
+			
+			if neighbour_in_direction.type != Gamemanager.TileType.WATER:
+				number_of_land_tiles += 1
+			
+			neighbour_in_direction.type = Gamemanager.TileType.WATER
+			
+			if step > 200 or neighbour_in_direction.type == Gamemanager.TileType.WATER and number_of_land_tiles > 5:
+				break
+	
+	return generated_map
