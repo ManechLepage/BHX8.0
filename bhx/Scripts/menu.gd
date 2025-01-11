026231ef4d85f33ad6fcc488ebf5a5524006265a
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2025-01-11 09:53:11 -0500

    gd

--- bhx/Scripts/menu.gd
+++ bhx/Scripts/menu.gd
@@ -2,8 +2,15 @@ extends Control
 
 @onready var game: Node2D = $"../../Game"
 @onready var control: Control = $"../Control"
+@onready var settings: Control = $"../Settings"
 
 func _ready() -> void:
 	game.visible = false
 	control.visible = false
 	visible = true
+	settings.visible = false
+
+
+func _on_button_2_pressed() -> void:
+	settings.visible = true
+	visible = false
