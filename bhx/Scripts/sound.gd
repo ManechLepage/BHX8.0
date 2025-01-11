5589ad0706b132e1504fb6103e38a940d4218c28
Stars4Allen <77178594+Stars4Allen@users.noreply.github.com>
2025-01-11 06:30:05 -0500

    BILL COLLECTO

--- bhx/Scripts/sound.gd
+++ bhx/Scripts/sound.gd
@@ -1,4 +1,5 @@
 extends Node
+@onready var money: AudioStreamPlayer = $money
 @onready var planesa: AudioStreamPlayer = $planesa
 @onready var jicleur: AudioStreamPlayer = $Jicleur
 @onready var planifere: AudioStreamPlayer = $planifere
@@ -8,3 +9,5 @@ func jicle() -> void:
 func plane() -> void:
 	planifere.play()
 	planesa.play()
+func dinero() -> void:
+	money.play()
